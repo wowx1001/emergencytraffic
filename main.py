31b5dc1a743008a5a756fc174ecb24e07dd90f3e
wowx1001 <wowx1001@naver.com>
2020-12-06 16:46:11 +0900

    디자인 수정

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import pymysql
 
 demo_db = pymysql.connect(
     user='root',
-    passwd='1111',
-    host='0.0.0.0',
+    passwd='tjrWkd2&',
+    host='127.0.0.1',
     db='demo',
     charset='utf8'
 )
@@ -48,5 +48,5 @@ def save_csv():
     return response
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True, threaded=True, port=5000)
+    app.run(debug=True, threaded=True)
 
